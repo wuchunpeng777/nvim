596a268df9634eb36c1121936fb4295ff869a564
wuchunpeng777 <wcpwcp9@gmail.com>
2024-05-23 14:11:45 +0800

    telescope快捷键

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -21,6 +21,10 @@ return { -- Fuzzy Finder (files, lsp, etc)
         'nvim-tree/nvim-web-devicons',
         enabled = vim.g.have_nerd_font
     }},
+    keys = {
+        {"<leader>fc",   "<cmd>lua require('telescope.builtin').lsp_document_symbols({show_line=true,symbols='method'})<cr>"},
+        {"<leader>fg",   "<cmd>lua require('telescope.builtin').lsp_workspace_symbols({show_line=true,symbols='method'})<cr>"},
+    },
     config = function()
         -- Telescope is a fuzzy finder that comes with a lot of different things that
         -- it can fuzzy find! It's more than just a "file finder", it can search
@@ -65,20 +69,21 @@ return { -- Fuzzy Finder (files, lsp, etc)
         -- See `:help telescope.builtin`
         local builtin = require 'telescope.builtin'
         -- vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-        vim.keymap.set('n', '<leader>sk', builtin.keymaps, {
+        vim.keymap.set('n', '<leader>fk', builtin.keymaps, {
             desc = '[S]earch [K]eymaps'
         })
-        vim.keymap.set('n', '<leader>sf', builtin.find_files, {
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, {
             desc = '[S]earch [F]iles'
         })
+
         -- vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
         -- vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-        vim.keymap.set('n', '<leader>sw', builtin.live_grep, {
+        vim.keymap.set('n', '<leader>fs', builtin.live_grep, {
             desc = '[S]earch by [G]rep'
         })
         -- vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
         -- vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-        vim.keymap.set('n', '<leader>sr', builtin.oldfiles, {
+        vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {
             desc = '[S]earch Recent Files ("." for repeat)'
         })
         vim.keymap.set('n', '<leader><leader>', builtin.buffers, {
@@ -86,7 +91,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
         })
 
         -- Slightly advanced example of overriding default behavior and theme
-        vim.keymap.set('n', '<leader>/', function()
+        vim.keymap.set('n', '<leader>fd', function()
             -- You can pass additional configuration to Telescope to change the theme, layout, etc.
             builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
                 winblend = 10,
