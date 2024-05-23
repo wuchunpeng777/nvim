9906ae192cead956856c695a4ee9f029731df98e
wuchunpeng777 <wcpwcp9@gmail.com>
2024-05-23 15:53:41 +0800

    字体

--- lua/global.lua
+++ lua/global.lua
@@ -13,6 +13,7 @@ vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
+vim.o.guifont = "JetBrainsMono Nerd Font:h12"
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -23,7 +24,7 @@ vim.g.have_nerd_font = true
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
-vim.opt.relativenumber = true
+-- vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
