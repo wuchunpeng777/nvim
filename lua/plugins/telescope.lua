7949a355b53913316ed89768018ef6b8b8a296ac
wuchunpeng777 <wcpwcp9@gmail.com>
2024-05-24 18:24:22 +0800

    telescope mapping

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -55,14 +55,18 @@ return { -- Fuzzy Finder (files, lsp, etc)
             -- You can put your default mappings / updates / etc. in here
             --  All the info you're looking for is in `:help telescope.setup()`
             --
-            -- defaults = {
-            --   mappings = {
-            --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-            --   },
-            -- },
             -- pickers = {}
             defaults = {
-                layout_strategy = 'vertical'
+                layout_strategy = 'vertical',
+              mappings = {
+i = {
+        -- map actions.which_key to <C-h> (default: <C-/>)
+        -- actions.which_key shows the mappings for your picker,
+        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+        ["<C-j>"] = "move_selection_next",
+        ["<C-k>"] = "move_selection_previous",
+      }
+              },
             },
             extensions = {
                 ['ui-select'] = {require('telescope.themes').get_dropdown()}
