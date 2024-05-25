122f143b51266d9abf185e25f44d120ce42ac7be
wuchunpeng777 <wcpwcp9@gmail.com>
2024-05-25 16:52:31 +0800

    session config

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -1,15 +1,16 @@
 return {
     'Shatur/neovim-session-manager',
-    config = function ()
-        --save session
+    config = function()
+        -- save session
         vim.keymap.set('n', '<leader>Ss', '<cmd>SessionManager save_current_session<cr>')
-        --load session
+        -- load session
         vim.keymap.set('n', '<leader>Sl', '<cmd>SessionManager load_session<cr>')
-        --delete session
+        -- delete session
         vim.keymap.set('n', '<leader>Sd', '<cmd>SessionManager delete_session<cr>')
 
         require('session_manager').setup({
-            autoload_mode = 'Disabled'
+            autoload_mode = 'Disabled',
+            autosave_last_session = false,
         })
     end
 }
