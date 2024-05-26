2672afe76161c6e5c76efc74a383578d4a66b2c7
wuchunpeng <wcpwcp9@gmail.com>
2024-05-26 21:54:24 +0800

    treesitter区分平台编译

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-        ensure_installed = {'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', "c_sharp"},
+        ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', "c_sharp" },
         -- Autoinstall languages that are not installed
         auto_install = true,
         highlight = {
@@ -10,17 +10,20 @@ return { -- Highlight, edit, and navigate code
             -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
             --  If you are experiencing weird indenting issues, add the language to
             --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-            additional_vim_regex_highlighting = {'ruby'}
+            additional_vim_regex_highlighting = { 'ruby' }
         },
         indent = {
             enable = true,
-            disable = {'ruby'}
+            disable = { 'ruby' }
         }
     },
     config = function(_, opts)
         -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
-        require'nvim-treesitter.install'.compilers = {"zig"}
+        if vim.fn.has "win32" == 1 then
+            require 'nvim-treesitter.install'.compilers = { "zig" }
+        end
+
         -- Prefer git instead of curl in order to improve connectivity in some environments
         require('nvim-treesitter.install').prefer_git = true
         ---@diagnostic disable-next-line: missing-fields
