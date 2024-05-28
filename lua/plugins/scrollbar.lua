340d9890e6b3f3a3c0b24617e50a3a7db11b90a9
wuchunpeng777 <wcpwcp9@gmail.com>
2024-05-28 20:05:54 +0800

    scrollbar config

--- lua/plugins/scrollbar.lua
+++ lua/plugins/scrollbar.lua
@@ -3,12 +3,7 @@ return {
     event = "BufEnter",
     config = function()
         require('scrollview').setup({
-            -- excluded_filetypes = { 'nerdtree' },
-            -- current_only = true,
-            -- base = 'buffer',
-            -- column = 80,
-            -- signs_on_startup = { 'all' },
-            -- diagnostics_severities = { vim.diagnostic.severity.ERROR }
+            current_only = true,
         })
     end
 }
