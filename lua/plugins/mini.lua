878469d63a2c9004619e66279f39a45701083696
wuchunpeng777 <wcpwcp9@gmail.com>
2024-05-29 17:52:11 +0800

    mini-files keymap

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -19,6 +19,7 @@ return { -- Collection of various small independent plugins/modules
         require('mini.surround').setup()
 
         require('mini.files').setup({})
+        vim.keymap.set('n', '<leader>fi', '<cmd>lua MiniFiles.open()<cr>')
 
         require('mini.starter').setup({
             -- Set to true if you have a Nerd Font
