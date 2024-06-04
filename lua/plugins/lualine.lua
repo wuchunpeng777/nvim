aa9fb32851c47f4fed112252d92ccae9c92a089e
wuchunpeng777 <wcpwcp9@gmail.com>
2024-06-04 16:41:49 +0800

    lualine显示宏录制状态

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,44 @@
 return {
     'nvim-lualine/lualine.nvim',
-    dependencies = {'nvim-tree/nvim-web-devicons'},
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
+        local function show_macro_recording()
+            local recording_register = vim.fn.reg_recording()
+            if recording_register == "" then
+                return ""
+            else
+                return "Recording @" .. recording_register
+            end
+        end
+        local lualine = require('lualine')
+        vim.api.nvim_create_autocmd("RecordingEnter", {
+            callback = function()
+                lualine.refresh({
+                    place = { "statusline" },
+                })
+            end,
+        })
+
+        vim.api.nvim_create_autocmd("RecordingLeave", {
+            callback = function()
+                -- This is going to seem really weird!
+                -- Instead of just calling refresh we need to wait a moment because of the nature of
+                -- `vim.fn.reg_recording`. If we tell lualine to refresh right now it actually will
+                -- still show a recording occuring because `vim.fn.reg_recording` hasn't emptied yet.
+                -- So what we need to do is wait a tiny amount of time (in this instance 50 ms) to
+                -- ensure `vim.fn.reg_recording` is purged before asking lualine to refresh.
+                local timer = vim.loop.new_timer()
+                timer:start(
+                    50,
+                    0,
+                    vim.schedule_wrap(function()
+                        lualine.refresh({
+                            place = { "statusline" },
+                        })
+                    end)
+                )
+            end,
+        })
         require('lualine').setup {
             options = {
                 icons_enabled = true,
@@ -28,18 +65,20 @@ return {
                 }
             },
             sections = {
-                lualine_a = {'mode'},
-                lualine_b = {'branch', 'diff', 'diagnostics'},
-                lualine_c = {'filename'},
-                lualine_x = {'encoding', 'fileformat', 'filetype'},
-                lualine_y = {'progress'},
-                lualine_z = {'location'}
+                lualine_a = { 'mode' },
+                lualine_b = { {
+                    'macro-recording', fmt = show_macro_recording
+                }, 'branch', 'diff', 'diagnostics' },
+                lualine_c = { 'filename' },
+                lualine_x = { 'encoding', 'fileformat', 'filetype' },
+                lualine_y = { 'progress' },
+                lualine_z = { 'location' }
             },
             inactive_sections = {
                 lualine_a = {},
                 lualine_b = {},
-                lualine_c = {'filename'},
-                lualine_x = {'location'},
+                lualine_c = { 'filename' },
+                lualine_x = { 'location' },
                 lualine_y = {},
                 lualine_z = {}
             },
