582f27e60d60de98db3dc9d3e7e49ef7be7c0850
wuchunpeng777 <wcpwcp9@gmail.com>
2024-06-11 11:41:37 +0800

    disable autosave message

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -1,9 +1,16 @@
 return {
-    "Pocco81/auto-save.nvim",
+    "okuuva/auto-save.nvim",
     config = function()
         require("auto-save").setup {
             -- your config goes here
             -- or just leave it empty :)
+            execution_message = {
+                enabled = false,
+                message = function() -- message to print on save
+                end,
+                dim = 0.18,   -- dim the color of `message`
+                cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+            },
         }
     end
 }
