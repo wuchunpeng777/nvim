1840ef303a9182e6d729bf392a15fb439b521e5c
wuchunpeng <wcpwcp9@gmail.com>
2024-06-11 22:47:48 +0800

    doc border

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -31,6 +31,7 @@ return { -- You can easily change to a different colorscheme.
         vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "none" })
 
         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
 
         vim.api.nvim_set_hl(0, "WhichKeyFloat", { bg = "none" })
 
