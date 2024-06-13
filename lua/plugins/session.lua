3eb625a70c8ef0747ff25c54439cc3a993bbc1f6
wuchunpeng777 <wcpwcp9@gmail.com>
2024-06-13 10:14:35 +0800

    config

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -8,9 +8,10 @@ return {
         -- delete session
         vim.keymap.set('n', '<leader>Sd', '<cmd>SessionManager delete_session<cr>')
 
+        local config = require('session_manager.config')
 
         require('session_manager').setup({
-            autoload_mode = 'Disabled',
+            autoload_mode = config.AutoloadMode.Disabled,
             autosave_last_session = true,
             autosave_ignore_not_normal = true,
         })
