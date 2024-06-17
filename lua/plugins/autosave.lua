9a962a3222c57fc4c0a585a40d6f424f1a2434fe
wuchunpeng777 <wcpwcp9@gmail.com>
2024-06-17 16:14:42 +0800

    autosave delay

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -12,6 +12,7 @@ return {
                 dim = 0.18,   -- dim the color of `message`
                 cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
             },
+            debounce_delay = 50
         }
     end
 }
